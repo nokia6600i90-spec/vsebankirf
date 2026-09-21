35f0394cd7d980293b4e6101c4480dac47fee25c
nokia6600i90-spec <nokia6600i90@gmail.com>
2026-09-22 00:37:37 +0300

    Update VbApp.kt

--- app/src/main/java/ru/vsebankirf/app/VbApp.kt
+++ app/src/main/java/ru/vsebankirf/app/VbApp.kt
@@ -1,5 +1,25 @@
 package ru.vsebankirf.app
 
 import android.app.Application
+import android.content.Intent
+import android.os.Process
+import java.io.PrintWriter
+import java.io.StringWriter
 
-class VbApp : Application()
+class VbApp : Application() {
+
+override fun onCreate() {
+super.onCreate()
+Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
+try {
+val sw = StringWriter()
+throwable.printStackTrace(PrintWriter(sw))
+val intent = Intent(this, CrashActivity::class.java)
+intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+intent.putExtra(CrashActivity.EXTRA_TRACE, sw.toString())
+startActivity(intent)
+} catch (e: Exception) {
+}
+Process.killProcess(Process.myPid())
+}
+}}
